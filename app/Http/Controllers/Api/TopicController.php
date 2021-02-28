5fe07a3e8e17028e6272d1cbb861fa1fa60aa8f2
akandeBolaji <akandebolajidanielofficial@gmail.com>
2021-02-28 23:01:18 +0100

    create publish method in TopicController

--- app/Http/Controllers/Api/TopicController.php
+++ app/Http/Controllers/Api/TopicController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Subscription;
 use App\Models\Topic;
+use App\Events\MessagePublished;
 
 class TopicController extends Controller
 {
@@ -47,6 +48,35 @@ class TopicController extends Controller
         return response()->json([
             'url' => $request->url,
             'topic' => $request->topic,
+        ], 200);
+    }
+
+    public function publish(Request $request, $topic)
+    {
+        $modified_request = new Request([
+            'topic' => $topic,
+            'data' => $request->all()
+        ]);
+
+        $validator = Validator::make($modified_request->all(), [
+            'data' => 'required',
+            'topic' => 'required|exists:topics,value'
+        ]);
+
+        if ($validator->fails()) {
+            $error = $validator->errors()->first();
+            return response()->json([
+                'error' => 'invalid_input',
+                'message' => $error,
+            ], 400);
+        }
+
+        $topic = Topic::where('value', $modified_request->topic)->first();
+
+        MessagePublished::dispatch($topic, $modified_request->all());
+
+        return response()->json([
+            'message' => 'Publish was successful',
         ], 200);
     }
 }
