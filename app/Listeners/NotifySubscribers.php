7d256516e0657eeaadd7db41efe28520e68e1a84
akandeBolaji <akandebolajidanielofficial@gmail.com>
2021-02-28 23:04:51 +0100

    generate message publish event and notify listener

---
+++ app/Listeners/NotifySubscribers.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\MessagePublished;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class NotifySubscribers
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  MessagePublished  $event
+     * @return void
+     */
+    public function handle(MessagePublished $event)
+    {
+        //
+    }
+}
